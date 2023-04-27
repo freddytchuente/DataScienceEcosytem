899b7e9bdbd1316bea1d7e792749006be11aeed2
freddytchuente <38319613+freddytchuente@users.noreply.github.com>
2023-04-27 20:32:20 +0200

    Add files via upload

---
+++ DataScienceEcosystem.ipynb.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objective__
+#     - List popular languages for Data Science
+#     - List popular libraries in Data Science 
+#     - Using of Git and Github
+# 
+
+# Some of the popular languages that Data Scientists use are:
+#     . Python
+#     . R
+#     . SQL
+#     . Julia
+
+# Some of the popular libraries that Data Scientists use are:
+#     . Tensorflow
+#     . NumPy
+#     . SciPy
+#     . Matplotlib
+#     . Keras
+
+# # Data science Tools
+
+# |Jupiter Notebook|
+# |RStudio|
+# |Apache Zeppelin|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# ## Author
+# Author: Arthur
+
+# In[ ]:
+
+
+
+
